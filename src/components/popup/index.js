bf428226bbe526de0c1950bb144d8752d3f745b3
Brenda Pham <blpham21@gmail.com>
2024-05-15 03:52:35 -0700

    change exit button icon on popup

--- src/components/popup/index.js
+++ src/components/popup/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 import "./popup.css";
-import PinkCircle from "../../assets/pinkcircle.png";
+import ExitIcon from "../../assets/exiticon.png";
 
 const Popup = (props) => {
   return props.trigger ? (
     <div className="popup">
       <div className="popup-inner">
         <button className="close-btn" onClick={() => props.setTrigger(false)}>
-          <img src={PinkCircle} />
+          <img src={ExitIcon} />
         </button>
         {props.children}
       </div>
